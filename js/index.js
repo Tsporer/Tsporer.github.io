da4a15417ab577c2a35bfac636583dc114d6a702
Tsporer <98298922+Tsporer@users.noreply.github.com>
2022-09-25 22:50:11 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-const navToggle = document.querySelector('.nav-toggle');
+/*const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-})
+console.log(navToggle);
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// })
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
-})
+})*/
