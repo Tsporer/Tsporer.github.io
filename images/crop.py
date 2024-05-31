ca2c11463008fe3a24776ebf87db0a7670900891
tsporer <tsporer@umich.edu>
2024-05-31 14:50:25 -0400

    Updates

--- images/crop.py
+++ images/crop.py
@@ -1,26 +1,22 @@
 from PIL import Image
 
 def crop_to_square(input_path, output_path, top_left_x, top_left_y):
-    # Open the image
+    # Open image
     image = Image.open(input_path)
     
-    # Get the dimensions of the image
+    # Get dimensions and crop
     width, height = image.size
-    
-    # Calculate the size of the square
     size = min(width - top_left_x, height - top_left_y)
-    
-    # Crop the image to a square
     cropped_image = image.crop((top_left_x, top_left_y, top_left_x + size, top_left_y + size))
     
-    # Save the cropped image to the output path
+    # Save image
     cropped_image.save(output_path)
 
 if __name__ == '__main__':
     input_image_path = 'robot.jpg'
     output_image_path = 'robot_cropped.jpg'
-    top_left_x = 63  # Change this to your desired x-coordinate
-    top_left_y = 0   # Change this to your desired y-coordinate
+    top_left_x = 63
+    top_left_y = 0
 
     crop_to_square(input_image_path, output_image_path, top_left_x, top_left_y)
 
